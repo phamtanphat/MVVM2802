4706ad9bee6f712673d30baf3e5e0dd85028833a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-02 18:15:01 +0700

    a

---
+++ app/src/main/java/khoapham/ptp/phamtanphat/mvvmpattern/User.java
@@ -0,0 +1,48 @@
+package khoapham.ptp.phamtanphat.mvvmpattern;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+
+public class User extends BaseObservable {
+    private String email;
+    private String password;
+    private Integer image;
+
+    public User(String email, String password, Integer image) {
+        this.email = email;
+        this.password = password;
+        this.image = image;
+    }
+
+    @Bindable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+        notifyPropertyChanged(BR.email);
+    }
+
+    @Bindable
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        notifyPropertyChanged(BR.password);
+    }
+
+    @Bindable
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+        notifyPropertyChanged(BR.image);
+    }
+}
