c40132b9461f58a680d5fb4d4512ef0a1122684e
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-02 18:44:11 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/mvvmpattern/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/mvvmpattern/MainActivity.java
@@ -1,13 +1,33 @@
 package khoapham.ptp.phamtanphat.mvvmpattern;
 
+import android.databinding.DataBindingUtil;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import khoapham.ptp.phamtanphat.mvvmpattern.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        final User user = new User("","",R.mipmap.ic_launcher);
+        mainBinding.setUser(user);
+        mainBinding.executePendingBindings();
+
+        // cap nhat cho 1 so thuoc tinh khong the hieu gia tri truyen vao
+        mainBinding.imageview.setImageResource(R.mipmap.ic_launcher);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB",user.getEmail());
+                handler.postDelayed(this,500);
+            }
+        },500);
+
     }
 }
